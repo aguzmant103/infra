7ce34240e89680185723e6e89ad8da79b122d618
aguzmant103 <67167307+aguzmant103@users.noreply.github.com>
2022-05-21 18:36:55 -0600

    Added initial Typescript project

---
+++ typescript-project/test/typescript-project.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as TypescriptProject from '../lib/typescript-project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/typescript-project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TypescriptProject.TypescriptProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
